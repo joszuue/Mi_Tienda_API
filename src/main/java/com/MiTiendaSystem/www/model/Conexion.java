b08ec51977aa64326747db8cc1fab33c81e20af4
joszuue <29vasquezz@gmail.com>
2024-08-19 18:14:57 -0600

    Archivo de conexión a la bdd

--- src/main/java/com/MiTiendaSystem/www/model/Conexion.java
+++ src/main/java/com/MiTiendaSystem/www/model/Conexion.java
@@ -6,7 +6,7 @@ import java.util.logging.Logger;
 
 public class Conexion {
 
-	 static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	 static final String JDBC_DRIVER = "com.mysql.jdbc.Driver ";
 	 static final String DB_URL = "jdbc:mysql://localhost:3306/mi_tienda_system";
 
 	 static final String USER = "root";
